29642376228c0ddc6d4d743b3dc4729ff7b609cb
rakhawicaksana <wicaksanarakhaa@gmail.com>
2026-09-22 15:25:47 +0700

    resource,view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/berita', function () {
+    return view('berita');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
